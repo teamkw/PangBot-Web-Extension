1b3a0d48dcd340aaf16b78beaa59e0a3de59ba7e
pkeugine <pkeugine@gmail.com>
2020-10-06 02:45:08 +0900

    refactor small button code

--- dark_mode.js
+++ dark_mode.js
@@ -8,13 +8,26 @@
     //document.body.style.color = "white";
     var i;
     var noticeButtons = document.querySelectorAll('.btn-lightgreen');
-    for (i = 0; i < noticeButtons.length; ++i) { noticeButtons[i].style.cssText = "background-color: " + darkModeLightGreen + ";"; }
+    for (i = 0; i < noticeButtons.length; ++i) {
+        var curButton = noticeButtons[i];
+        curButton.style.cssText = "background-color: " + darkModeLightGreen + ";"; 
+    }
     var documentButtons = document.querySelectorAll('.btn-lightpurple');
-    for (i = 0; i < documentButtons.length; ++i) { documentButtons[i].style.cssText = "background-color: " + darkModeLightPurple + ";"; }
+    for (i = 0; i < documentButtons.length; ++i) {
+        var curButton = documentButtons[i];
+        curButton.style.cssText = "background-color: " + darkModeLightPurple + ";";
+    }
     var qnaButtons = document.querySelectorAll('.btn-lightbrown');
-    for (i = 0; i < qnaButtons.length; ++i) { qnaButtons[i].style.cssText = "background-color: " + darkModeLightBrown + ";"; }
+    for (i = 0; i < qnaButtons.length; ++i) {
+        var curButton = qnaButtons[i];
+        curButton.style.cssText = "background-color: " + darkModeLightBrown + ";";
+    }
     var toktokButtons = document.querySelectorAll('.btn-lightblue');
-    for (i = 0; i < toktokButtons.length; ++i) { toktokButtons[i].style.cssText = "background-color: " + darkModeLightBlue + ";"; }
+    for (i = 0; i < toktokButtons.length; ++i) {
+        var curButton = toktokButtons[i];
+        curButton.style.cssText = "background-color: " + darkModeLightBlue + ";"; 
+    }
     document.getElementsByClassName("bg-light")[0].style.cssText = "background-color: " + darkModeBlack + " !important";
     document.getElementsByClassName("bg-top")[0].style.cssText = "background-color: " + darkModeKw + " !important";
+    
 })();
